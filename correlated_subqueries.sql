9b32c3764cb1a9a71d15255676cf4c76afeaa3a0
OwenShuaiLiu <34903596+OwenShuaiLiu@users.noreply.github.com>
2020-04-27 00:29:14 -0700

    classic models

---
+++ correlated_subqueries.sql
@@ -0,0 +1,6 @@
+use classicmodels;
+
+Who reports to Mary Patterson?
+Which payments in any month and year are more than twice the average for that month and year (i.e. compare all payments in Oct 2004 with the average payment for Oct 2004)? Order the results by the date of the payment. You will need to use the date functions.
+Report for each product, the percentage value of its stock on hand as a percentage of the stock on hand for product line to which it belongs. Order the report by product line and percentage value within product line descending. Show percentages with two decimal places.
+For orders containing more than two products, report those products that constitute more than 50% of the value of the order.
